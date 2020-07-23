28fffdc6acad79dd1af83efe1bf2d38bf1549dd5
patriciatamega <pat.tamega@gmail.com>
2020-07-23 11:39:30 -0300

    Constantes e Array Constantes

---
+++ exemplo-01.php
@@ -0,0 +1,9 @@
+<?php
+
+// Constante
+
+define("SERVIDOR", "127.0.0.1");
+
+echo SERVIDOR;
+
+?>
---
+++ exemplo-02.php
@@ -0,0 +1,14 @@
+<?php
+
+// Array Constante
+
+define("BANCO_DE_DADOS", [
+    '127.0.0.1',
+    'root',
+    'password',
+    'test'
+]);
+
+print_r(BANCO_DE_DADOS);
+
+?>
